29e981549e12d2063feb52bc67bc1b2345639dc2
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-10 11:11:45 -0500

    fill out some of the user layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'home'  => 'pretty_pictures#home'
-  get 'login' => 'pretty_pictures#login'
+
+  get 'login' => 'users#login'
+  get 'signup' => 'users#new'
 
   root 'pretty_pictures#home'
 end
