1c35217e2694248f03a42dbdd843bd588e04c3ee
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-10 11:49:54 -0500

    add users database table
    I didn't add the password right now, as I'm not sure how I'll store the
    hash and salt quite yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151210164838) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
