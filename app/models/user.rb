6eed739a42e336b5b21ccd154277abf29cf86085
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-10 13:35:13 -0500

    normalize usernames to lower case
    get a head start on that case insensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   string_field_length = {maximum: 63}
+
+  before_save { self.username = username.downcase }
+
   validates :username, presence: true, length: string_field_length, uniqueness: true
   validates :token, length: string_field_length
   validates :secret, length: string_field_length
