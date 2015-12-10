55f439bbd951359aa34d8802305cfc6c5add1416
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-10 13:27:09 -0500

    add validations to user model
    Makes the tests pass, and the data have integrity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  string_field_length = {maximum: 63}
+  validates :username, presence: true, length: string_field_length, uniqueness: true
+  validates :token, length: string_field_length
+  validates :secret, length: string_field_length
+  validates :bio, length: {maximum: 255}
 end
