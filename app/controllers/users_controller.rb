c34c81e7de93b8a78baeef6444ef133b9261b089
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-10 15:56:32 -0500

    add the signup form
    No error handling, yet.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'show', @user.id
+    else
+      render 'new'
+    end
+  end
+
   def login
   end
+
   def show
     @user= User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :username, :password, :password_confirmation, :bio)
+    end
 end
