ae91495b57751b083506d1ed7331ba257e4c09ab
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-11 16:36:39 -0500

    complete the login action
    Logs them in and redirects to user profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       message = "You are logged in. Welcome, #{user.username}"
       flash[:success] = message
-      redirect_to home_path
+      redirect_to user
     else
       message = 'Oops, looks like your username or password are incorrect.'
       flash.now[:danger] = message
