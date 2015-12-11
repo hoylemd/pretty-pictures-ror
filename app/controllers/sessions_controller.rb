289e55067cca49cdcb93d2ea37bb74754157dc9f
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-11 15:08:59 -0500

    implement non-persistent login
    This doesn't *really* log the user in, as it doesn't create a session
    yet, but it's enough for the front-end functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      message = "You are logged in. Welcome, #{user.username}"
+      flash[:success] = message
+      redirect_to home_path
+    else
+      message = 'Oops, looks like your username or password are incorrect.'
+      flash.now[:danger] = message
+      render 'new'
+    end
   end
 
   def destroy
