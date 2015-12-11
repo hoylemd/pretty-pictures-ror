35c29b70090834ed2ba69342eb34106a84bed4e0
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-11 13:43:34 -0500

    move my login view placeholder to the correct file
    since logging in is no longer part of the user resource, it shouldn't
    live in the user views/controller

--- app/controllers/pretty_pictures_controller.rb
+++ app/controllers/pretty_pictures_controller.rb
@@ -1,7 +1,4 @@
 class PrettyPicturesController < ApplicationController
   def home
   end
-
-  def login
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-  end
-
   def show
     @user= User.find(params[:id])
   end
