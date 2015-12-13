2dd78ba0450f24d872cab6a187d43c6cc1b7df86
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-13 18:07:46 -0500

    request and display the top 100 pictures
    Yay! it's working!

--- app/controllers/pretty_pictures_controller.rb
+++ app/controllers/pretty_pictures_controller.rb
@@ -3,6 +3,8 @@ class PrettyPicturesController < ApplicationController
     redirect_to login_path unless logged_in?
     redirect_to connect_path unless @current_user.connected_to_500px?
 
-    @api = build_api(@current_user.oauth_token, @current_user.oauth_secret)
+    api = build_api(@current_user.oauth_token, @current_user.oauth_secret)
+    path = '/v1/photos.json?feature=pupular&rpp=100'
+    @photos = MultiJson.decode(api.get(path).body)['photos']
   end
 end
