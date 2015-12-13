06b6fcbc1ec781f9e5839c8071e5f9941f11b87d
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-13 15:13:38 -0500

    Move the actual display logic into the template
    I'll want to use that helper method in a ruby code context very soon,
    (when redirecting to /connect if the user isn't connected) so I need it to
    return true or false.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def connected_to_500px?(user)
-    (user.oauth_token.blank? or user.oauth_secret.blank?) ? 'No' : 'Yes'
+    user.oauth_token.blank? or user.oauth_secret.blank?
   end
 end
