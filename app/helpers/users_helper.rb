76f0ae87e8f79b25aa626a0492392e5dcfb12130
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-13 15:12:02 -0500

    fix the connected indicator
    Using the wrong attribute names, and also the method name should ahve
    had a ?

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def connected_to_500px(user)
-    (user.token.blank? or @user.secret.blank) ? 'No' : 'Yes'
+  def connected_to_500px?(user)
+    (user.oauth_token.blank? or user.oauth_secret.blank?) ? 'No' : 'Yes'
   end
 end
