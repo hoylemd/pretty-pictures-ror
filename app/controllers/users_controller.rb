edeff54d728a813360c3ac839eaa50e1776939e1
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-13 14:42:30 -0500

    Add the actual oauth code
    This code was blatantly ripped from the (example
    code)[https://github.com/500px/api-documentation/blob/master/examples/Ruby/xauth.rb]
    
    I'm going to actually store the access token and secret, and build up a
    new Oauth consumer object for every request.  Obviously, asking users
    for their 500px credentials in my app instead of doing 3-legged
    authentication is sketchy and terrible, but I spent a good day and a
    half trying to do it 'right', without much in the way of success.  I
    would *never ever* do this in a real app, but it gets the authentication
    stuff out of the way/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create_connection
     redirect_to login_path unless logged_in?
 
-    @credentials = fivehundredpx_params
+    @access_token = get_access_token(fivehundredpx_params)
 
     #redirect_to home_path
   end
@@ -44,9 +44,35 @@ class UsersController < ApplicationController
     end
 
     def fivehundredpx_params
-      {
-        username: params.require(:username),
+      return {
+        username:params.require(:username),
         password: params.require(:password)
       }
+    end
+
+    BASE_URL = 'https://api.500px.com'
+    CONSUMER_KEY = 'qAONVEkKAQlbJBnyXXRmJdRN85QCfxxQlJQOjNzQ'
+    CONSUMER_SECRET = 'bwdE2F4GiGwVHDrccstzdq4CPsi6q91OSdhrGcMs'
+
+    # This is blatantly copied from the example apps in the 500px API docs
+    # well, with some of my own style changes
+    def get_access_token(credentials)
+      site_hash = {
+        site: BASE_URL,
+        request_token_path: "/v1/oauth/request_token",
+        access_token_path: "/v1/oauth/access_token",
+        authorize_path: "/v1/oauth/authorize"
+      }
+
+      consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, site_hash)
+
+      auth_hash = {
+        x_auth_mode: 'client_auth',
+        x_auth_username: credentials[:username],
+        x_auth_password: credentials[:password]
+      }
+
+      request_token = consumer.get_request_token()
+      consumer.get_access_token(request_token, {}, auth_hash)
     end
 end
