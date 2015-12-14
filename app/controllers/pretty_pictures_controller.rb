eb5adcd171b1eb80eca6ebbaabbd389ba409dbdc
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-14 00:22:33 -0500

    let un authenticated users see Home
    This also proves that the unauthenticated requests work!

--- app/controllers/pretty_pictures_controller.rb
+++ app/controllers/pretty_pictures_controller.rb
@@ -1,7 +1,8 @@
 class PrettyPicturesController < ApplicationController
   def home
-    redirect_to login_path unless logged_in?
-    redirect_to connect_path unless @current_user.connected_to_500px?
+    if logged_in? && ! @current_user.connected_to_500px?
+      redirect_to connect_path
+    end
 
     adaptor = FiveHundredPxAdaptor.new(@current_user)
     @photos = adaptor.get_top_100
