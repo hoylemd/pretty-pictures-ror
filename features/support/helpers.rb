4e03991720d67b93e75f281586a7ac11cb8ab256
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-14 15:13:44 -0500

    move the random string helpers into helpers
    They really arent coupled to signup, so they shouldn't be in the signup
    steps file

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -46,3 +46,40 @@ def assert_element_present(selector, message=nil, allow_ambiguous=false,
   end
   raise AssertionFailed, message unless found.empty?
 end
+
+def random_string(length=8, options)
+  # Generates a random string of `length` length
+  # options:
+  #  lower_case: boolean. set to false to exclude lower case characters
+  #              default: true
+  #  upper_case: boolean. set to false to exclude upper case characters
+  #              default: true
+  #  numbers: boolean. set to false to exclude numberic characters
+  #              default: true
+  #  special: boolean. set to false to exclude special characters ( _?&)
+  #              default: true
+
+  default_classes = {
+    lower_case: true, upper_case: true, numbers: true, special: false
+  }
+  use = default_classes.merge(options || {})
+
+  characters = []
+  characters += ('a'..'z').map { |i| i } if use[:lower_case]
+  characters += ('A'..'Z').map { |i| i } if use[:upper_case]
+  characters += ('0'..'9').map { |i| i } if use[:numbers]
+  characters += [' ', '_', '?', '&']  if use[:special]
+
+  if characters.empty?
+    raise Exception, "you need to choose at least one character class!"
+  end
+
+  (0...length).map { characters[rand(characters.length)] }.join
+end
+
+def fill_in_random(field, prefix=nil, string_options=nil)
+  random = random_string string_options
+  string = prefix.nil? ? "#{random}" : "#{prefix}:#{random}"
+  fill_in field, with: string
+  string
+end
