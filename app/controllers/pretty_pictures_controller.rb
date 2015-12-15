dda95fe5cf6931806b416aaa223f957febefa065
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-14 21:26:26 -0500

    Allow not connected users to view photos
    Also, add a prompt to connect on the photo pages

--- app/controllers/pretty_pictures_controller.rb
+++ app/controllers/pretty_pictures_controller.rb
@@ -1,10 +1,6 @@
 class PrettyPicturesController < ApplicationController
   def home
-    if logged_in? && ! @current_user.connected_to_500px?
-      redirect_to connect_path
-    end
-
-    adaptor = FiveHundredPxAdaptor.new(@current_user)
+    adaptor = FiveHundredPxAdaptor.new(current_user)
     @photos = adaptor.get_top_100
   end
 end
